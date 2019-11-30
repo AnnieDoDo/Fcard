38aeda29f0e2f7bd97a579397d7e36d0368c0f2c
AnnieDoDo <r8323467r@gmail.com>
2019-11-30 17:02:04 +0800

    change redis connect to redislab host

--- Fserver.js
+++ Fserver.js
@@ -6,16 +6,28 @@ const RedisStore = require('connect-redis')(session);
 const cors = require('cors')
 const bodyParser  = require('body-parser');
 const expressAccessToken = require('express-access-token');
-const client = redis.createClient();
+const nconf = require('nconf');
+nconf.argv().env().file('key.json');
+
+const client = redis.createClient(
+  nconf.get('redisPort') || '6379',
+  nconf.get('redisHost') || '127.0.0.1',
+  {
+    auth_pass: nconf.get('redisKey'),
+    return_buffers: true,
+  }
+)
+.on('error', err => console.error('ERR:REDIS:', err));
 const sql = require('./Fsearch');
 const bcrypt = require('bcrypt');
 
-const PORT = 8080;
+
+const PORT = 8081;
 const HOST = 'localhost';
 const app = express();
 
 app.use(session({
-    store: new RedisStore({ host: 'localhost', port: 6379, client: client}),
+    store: new RedisStore({client}),
     secret: 'dodo',
     saveUninitialized: false,
     resave: false,
